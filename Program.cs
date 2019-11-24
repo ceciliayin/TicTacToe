096b12ba4cad0b262606ea050aef85c9b34a05dc
cecilia.yin <cecilia.yin@myob.com>
2019-11-25 09:50:59 +1300

    Refactor

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace Tic_Tac_Toe
 { 
     class TicTacToe
     {
-        static char[] boardArray = {'.', '.', '.','.','.', '.', '.','.','.','.'};
+        private static readonly char[] BoardArray = {'.', '.', '.','.','.', '.', '.','.','.','.'}; 
         private static string _position;
-        static char Player1 = char.Parse("X");
-        static char Player2 = char.Parse("O");
+        private const char Player1 = 'X';
+        private const char Player2 = 'O';
 
         static void Main(string[] args)
         {
@@ -18,28 +18,29 @@ namespace Tic_Tac_Toe
 
             do
             {
-                Console.WriteLine("\n");
-                Console.WriteLine("Welcome to Tic Tac Toe!");
-                Console.WriteLine("Here's the current board:");
+                PrintTheMessage("\n");
+                PrintTheMessage("Welcome to Tic Tac Toe!");
+                PrintTheMessage("Here's the current board:");
                 PrintBoard();
 
                 if (roundCounter % 2 == 0)
                 {    
-                    Console.WriteLine("Player 2 enter a coord x,y to place your X or enter 'q' to give up: ");
+                    PrintTheMessage("Player 2 enter a coord x,y to place your X or enter 'q' to give up: ");
                 }
 
                 else
                 {
-                    Console.WriteLine("Player 1 enter a coord x,y to place your X or enter 'q' to give up: ");
+                    PrintTheMessage("Player 1 enter a coord x,y to place your X or enter 'q' to give up: ");
                 }
                 
                 _position = Console.ReadLine(); 
+                
                 var choiceOfPlayer = GetArrayIndex();
 
                 if (choiceOfPlayer == 10)
                 {
-                    Console.WriteLine("The position coordinate is invalid, try again......");
-                    Console.WriteLine("\n");
+                    PrintTheMessage("The position coordinate is invalid, try again......");
+                    PrintTheMessage("\n");
                     Thread.Sleep(2000);
                 }
 
@@ -50,29 +51,29 @@ namespace Tic_Tac_Toe
                     roundCounter=roundCounter+2;
                 }
 
-                if (choiceOfPlayer != 0 && boardArray[choiceOfPlayer] != Player1 && boardArray[choiceOfPlayer] != Player2 )
+                if (choiceOfPlayer != 0 && BoardArray[choiceOfPlayer] != Player1 && BoardArray[choiceOfPlayer] != Player2 )
                 {
                     if (roundCounter % 2 == 0)
                     {
-                        boardArray[choiceOfPlayer] = Player2;
+                        BoardArray[choiceOfPlayer] = Player2;
                     }
 
                     else
                     {
-                        boardArray[choiceOfPlayer] = Player1;
+                        BoardArray[choiceOfPlayer] = Player1;
                     }
                     
-                    Console.WriteLine("Move accepted, here's the current board: ");
+                    PrintTheMessage("Move accepted, here's the current board: ");
                     PrintBoard();
                     roundCounter++;
                 }
-                else if (boardArray[choiceOfPlayer] == Player1 && choiceOfPlayer !=0)
+                else if (BoardArray[choiceOfPlayer] == Player1 && choiceOfPlayer !=0)
                 { 
-                    Console.WriteLine("Oh no, a piece is already at this place! Try again..");
+                    PrintTheMessage("Oh no, a piece is already at this place! Try again..");
                 }
-                else if (boardArray[choiceOfPlayer] == Player2 && choiceOfPlayer !=0)
+                else if (BoardArray[choiceOfPlayer] == Player2 && choiceOfPlayer !=0)
                 { 
-                    Console.WriteLine("Oh no, a piece is already at this place! Try again..");
+                    PrintTheMessage("Oh no, a piece is already at this place! Try again..");
                 }
                 
                 result = CheckIfThePlayerWin();
@@ -91,101 +92,62 @@ namespace Tic_Tac_Toe
 
                 if (result == -1)
                 {
-                    Console.WriteLine("Draw");
+                    PrintTheMessage("Draw");
                 }
             }
         }
 
+        private static void PrintTheMessage(string message)
+        { 
+            Console.WriteLine(message);
+        }
         private static void PrintBoard()
         {
-            Console.WriteLine("  {0}    {1}    {2}", boardArray[1], boardArray[2], boardArray[3]);
-            Console.WriteLine("  {0}    {1}    {2}", boardArray[4], boardArray[5], boardArray[6]);
-            Console.WriteLine("  {0}    {1}    {2}", boardArray[7], boardArray[8], boardArray[9]);
+            Console.WriteLine("  {0}    {1}    {2}", BoardArray[1], BoardArray[2], BoardArray[3]);
+            Console.WriteLine("  {0}    {1}    {2}", BoardArray[4], BoardArray[5], BoardArray[6]);
+            Console.WriteLine("  {0}    {1}    {2}", BoardArray[7], BoardArray[8], BoardArray[9]);
         }
 
         private static int CheckIfThePlayerWin()
-        {
+        {    
             //check the horizontal condition
-            if (boardArray[1] == Player1 && boardArray[2] == Player1 && boardArray[3]==Player1)
+            int i = 1;
+            for (i = 1; i < 8;i += 3)
             {
-                return 1;
+                if ((BoardArray[i] == Player1 && BoardArray[i + 1] == Player1 && BoardArray[i + 2] == Player1)||(BoardArray[i] == Player2 && BoardArray[i + 1] == Player2 && BoardArray[i + 2] == Player2))
+                {
+                    return 1;
+                }
+
             }
             
-            if (boardArray[1] == Player2 && boardArray[2] == Player2 && boardArray[3]==Player2)
-            {
-                return 1;
-            }
-
-            if (boardArray[4] == Player1 && boardArray[5] == Player1 && boardArray[6] == Player1)
-            {
-                return 1;
-            }
-            
-            if (boardArray[4] == Player2 && boardArray[5] == Player2 && boardArray[6] == Player2)
-            {
-                return 1;
-            }
-
-            if (boardArray[7] == Player1 && boardArray[8] == Player1 && boardArray[9] == Player1)
-            {
-                return 1;
-            }
-            
-            if (boardArray[7] == Player2 && boardArray[8] == Player2 && boardArray[9] == Player2)
-            {
-                return 1;
-            }
-
             //check the vertical condition
-            if (boardArray[1] == Player1 &&  boardArray[4] ==Player1 && boardArray[7] == Player1)
+            for (i = 1; i < 4; i++)
             {
-                return 1;
-            }
-            if (boardArray[1] == Player2 &&  boardArray[4] ==Player2 && boardArray[7] == Player2)
-            {
-                return 1;
-            }
-
-            if (boardArray[2] == Player1 && boardArray[5] ==Player1 && boardArray[8] == Player1)
-            {
-                return 1;
+                if ((BoardArray[i] == Player1 && BoardArray[i + 3] == Player1 && BoardArray[i + 6] == Player1)||(BoardArray[i] == Player2 && BoardArray[i + 3] == Player2 && BoardArray[i + 6] == Player2))
+                {
+                    return 1;
+                }
+                
             }
             
-            if (boardArray[2] == Player2 && boardArray[5] ==Player2 && boardArray[8] == Player2)
+            //check diagonal condition
+            for (i = 1; i < 2; i ++)
             {
-                return 1;
+                if ((BoardArray[i] == Player1 && BoardArray[i + 4] == Player1 && BoardArray[i + 8] == Player1) || (BoardArray[i] == Player2 && BoardArray[i + 4] == Player2 && BoardArray[i + 8] == Player2))
+                    return 1;
+                
             }
 
-            if (boardArray[3] == Player1 && boardArray[6] ==Player1 && boardArray[9] == Player1)
+            for (i = 3; i < 4; i++)
             {
-                return 1;
-            }
-            if (boardArray[3] == Player2 && boardArray[6] ==Player2 && boardArray[9] == Player2)
-            {
-                return 1;
+                if ((BoardArray[i] == Player2 && BoardArray[i + 2] == Player2 && BoardArray[i + 4] == Player2) || (BoardArray[i] == Player1 && BoardArray[i + 2] == Player1 && BoardArray[i + 4] == Player1))
+                    return 1;
             }
 
-            //check the diagonal condition
-            if (boardArray[1] == Player1 && boardArray[5] == Player1 && boardArray[9]==Player1)
-            {
-                return 1;
-            }
-            if (boardArray[1] == Player2 && boardArray[5] == Player2 && boardArray[9]==Player2)
-            {
-                return 1;
-            }
 
-            if (boardArray[3] == Player1 && boardArray[5]==Player1 && boardArray[7] == Player1)
-            {
-                return 1;
-            }
-            
-            if (boardArray[3] == Player2 && boardArray[5]==Player2 && boardArray[7] == Player2)
-            {
-                return 1;
-            }
             //check if it's a draw
-            if (boardArray[1] != '.' && boardArray[2] != '.' && boardArray[3] != '.' && boardArray[4] != '.' && boardArray[5] != '.' && boardArray[6] != '.' && boardArray[7] != '.' && boardArray[8] != '.' && boardArray[9] != '.')
+            if (BoardArray[1] != '.' && BoardArray[2] != '.' && BoardArray[3] != '.' && BoardArray[4] != '.' && BoardArray[5] != '.' && BoardArray[6] != '.' && BoardArray[7] != '.' && BoardArray[8] != '.' && BoardArray[9] != '.')
             {
                 return -1;
             } 
