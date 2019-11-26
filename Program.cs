2bfec4a81165ab51296b43fc5e1a41c627b0677b
cecilia.yin <cecilia.yin@myob.com>
2019-11-26 16:20:40 +1300

    init commit

--- Program.cs
+++ Program.cs
@@ -2,135 +2,190 @@
 using System.Net;
 using System.Threading;
 
-namespace Tic_Tac_Toe
-{
-    class TicTacToe
-    { 
-        static void Main(string[] args)
+namespace TicTacToeOOP
+{ 
+    static class Program 
+    {
+        public static void Main(string[] args)
         {
             int result;
             int roundCounter = 1;
-            char[] boardArray = {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'};
+
+            Board newBoard = new Board();
+            Print printTheMessage=new Print();
+            GameRules newGameRules= new GameRules();
+            
+            printTheMessage.PrintTheMessage("Welcome to Tic Tac Toe!");
+            printTheMessage.PrintTheMessage("\n");
+            printTheMessage.PrintTheMessage("Here's the current board:");
+            printTheMessage.PrintTheMessage("\n");
+            newBoard.PrintBoard(newBoard.BoardArray);
 
             do
             {
-                const char player1 = 'X';
-                const char player2 = 'O';
+                Players player = new Players();
 
-                PrintTheMessage("Welcome to Tic Tac Toe!");
-                PrintTheMessage("Here's the current board:");
-                PrintBoard(boardArray);
-                PrintWhichPlayerShouldPlay(roundCounter);
-
+                printTheMessage.PrintTheMessage("\n");
+                printTheMessage.PrintWhichPlayerShouldPlay(roundCounter);
                 string position = Console.ReadLine();
-                int choiceOfPlayer = GetArrayIndex(position);
+                int choiceOfPlayer = player.GetArrayIndex(position);
 
                 if (choiceOfPlayer == 10)
                 {
-                    PrintTheMessage("The position coordinate is invalid, try again......");
-                    PrintTheMessage("\n");
+                    printTheMessage.PrintTheMessage("The position coordinate is invalid, try again......");
+                    printTheMessage.PrintTheMessage("\n");
                     Thread.Sleep(2000);
                 }
 
-                if ((boardArray[choiceOfPlayer] == player1 && choiceOfPlayer != 0) || (boardArray[choiceOfPlayer] == player2 && choiceOfPlayer != 0))
+                if ((newBoard.BoardArray[choiceOfPlayer] == player.Player1 && choiceOfPlayer != 0) || (newBoard.BoardArray[choiceOfPlayer] == player.Player2 && choiceOfPlayer != 0))
                 {
-                    PrintTheMessage("Oh no, a piece is already at this place! Try again..");
+                    printTheMessage.PrintTheMessage("Oh no, a piece is already at this place! Try again..");
+                    printTheMessage.PrintTheMessage("\n");
                     roundCounter++;
                 }
 
-                UpdateTokens(player1, player2, boardArray, roundCounter, choiceOfPlayer);
+                player.UpdateTokens(player.Player1, player.Player2, newBoard.BoardArray, roundCounter, choiceOfPlayer);
+                newBoard.PrintBoard(newBoard.BoardArray);
                 roundCounter++;
-                
-                result = CheckIfThePlayerWin(player1, player2, boardArray);
-            }while (result != 1 && result != -1);
+                result = newGameRules.CheckIfThePlayerWin(player.Player1, player.Player2, newBoard.BoardArray);
+
+            } while (result==0);
+            {   
+                //Console.Clear();
+                newBoard.PrintBoard(newBoard.BoardArray); 
+                newGameRules.CheckIfConditionIsWinning(result,roundCounter,newBoard.BoardArray);
+            }
+        }
+    }
+}
+public class Print
+{
+    public void PrintTheMessage(string message)
+    {
+        Console.Write(message);
+    }
+
+    public void PrintWhichPlayerShouldPlay(int roundCounter)
+    {
+        if (roundCounter % 2 == 0)
+        {
+            PrintTheMessage("Player 2 enter a coord x,y to place your X or enter 'q' to give up: ");
+        }
+
+        else
+        {
+            PrintTheMessage("Player 1 enter a coord x,y to place your X or enter 'q' to give up: ");
+        }
+    }
+}
+
+public class Board {
+
+    private char[] _boardArray = {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'};
+    public char[] BoardArray
+    { 
+        get
+        {
+            return _boardArray;
+        }
+    }
+    public void PrintBoard(char[] boardArray)
+    {
+        Console.WriteLine("  {0}    {1}    {2}", _boardArray[1], _boardArray[2], _boardArray[3]);
+        Console.WriteLine("  {0}    {1}    {2}", _boardArray[4], _boardArray[5], _boardArray[6]);
+        Console.WriteLine("  {0}    {1}    {2}", _boardArray[7], _boardArray[8], _boardArray[9]);
+    }
+}
+
+public class GameRules
+{ 
+    public int CheckIfThePlayerWin(char player1, char player2, char[] boardArray)
+    {    
+        //check if it's a draw
+        if (boardArray[1] != '.' && boardArray[2] != '.' && boardArray[3] != '.' && boardArray[4] != '.' &&
+            boardArray[5] != '.' && boardArray[6] != '.' && boardArray[7] != '.' && boardArray[8] != '.' &&
+            boardArray[9] != '.')
+        {
+            return -1;
+        }
+        
+        //check the horizontal condition
+        int i = 1;
+        for (i = 1; i < 8; i += 3)
+        {
+            if ((boardArray[i] == player1 && boardArray[i + 1] == player1 && boardArray[i + 2] == player1) ||
+                (boardArray[i] == player2 && boardArray[i + 1] == player2 && boardArray[i + 2] == player2))
             {
-                CheckIfConditionIsWinning(result, roundCounter, boardArray);
+                return 1;
             }
         }
 
-        private static void PrintTheMessage(string message)
+        //check the vertical condition
+        for (i = 1; i < 4; i++)
         {
-            Console.WriteLine(message);
-        }
-
-        private static void PrintTheMessageWithNumber(string message, int roundCounter)
-        {
-            Console.WriteLine(message, roundCounter);
-        }
-
-        private static void PrintWhichPlayerShouldPlay(int roundCounter)
-        {
-            if (roundCounter % 2 == 0)
+            if ((boardArray[i] == player1 && boardArray[i + 3] == player1 && boardArray[i + 6] == player1) ||
+                (boardArray[i] == player2 && boardArray[i + 3] == player2 && boardArray[i + 6] == player2))
             {
-                PrintTheMessage("Player 2 enter a coord x,y to place your X or enter 'q' to give up: ");
-            }
-
-            else
-            {
-                PrintTheMessage("Player 1 enter a coord x,y to place your X or enter 'q' to give up: ");
+                return 1;
             }
         }
 
-        private static void PrintBoard(char[] boardArray)
+        //check diagonal condition
+        for (i = 1; i < 2; i++)
         {
-            Console.WriteLine("  {0}    {1}    {2}", boardArray[1], boardArray[2], boardArray[3]);
-            Console.WriteLine("  {0}    {1}    {2}", boardArray[4], boardArray[5], boardArray[6]);
-            Console.WriteLine("  {0}    {1}    {2}", boardArray[7], boardArray[8], boardArray[9]);
+            if ((boardArray[i] == player1 && boardArray[i + 4] == player1 && boardArray[i + 8] == player1) ||
+                (boardArray[i] == player2 && boardArray[i + 4] == player2 && boardArray[i + 8] == player2))
+                return 1;
+
         }
 
-        private static int CheckIfThePlayerWin(char player1, char player2, char[] boardArray)
+        for (i = 3; i < 4; i++)
         {
-            //check the horizontal condition
-            int i = 1;
-            for (i = 1; i < 8; i += 3)
-            {
-                if ((boardArray[i] == player1 && boardArray[i + 1] == player1 && boardArray[i + 2] == player1) ||
-                    (boardArray[i] == player2 && boardArray[i + 1] == player2 && boardArray[i + 2] == player2))
-                {
-                    return 1;
-                }
-            }
+            if ((boardArray[i] == player2 && boardArray[i + 2] == player2 && boardArray[i + 4] == player2) ||
+                (boardArray[i] == player1 && boardArray[i + 2] == player1 && boardArray[i + 4] == player1))
+                return 1;
+        }
+        
+        return 0; 
+    }
+    
+    public void CheckIfConditionIsWinning(int result, int roundCounter, char[] boardArray)
+    {   
+        Console.Clear();
+        Print printTheMessage=new Print();
+        Board newBoard = new Board();
+        
 
-            //check the vertical condition
-            for (i = 1; i < 4; i++)
-            {
-                if ((boardArray[i] == player1 && boardArray[i + 3] == player1 && boardArray[i + 6] == player1) ||
-                    (boardArray[i] == player2 && boardArray[i + 3] == player2 && boardArray[i + 6] == player2))
-                {
-                    return 1;
-                }
-            }
-
-            //check diagonal condition
-            for (i = 1; i < 2; i++)
-            {
-                if ((boardArray[i] == player1 && boardArray[i + 4] == player1 && boardArray[i + 8] == player1) ||
-                    (boardArray[i] == player2 && boardArray[i + 4] == player2 && boardArray[i + 8] == player2))
-                    return 1;
-
-            }
-
-            for (i = 3; i < 4; i++)
-            {
-                if ((boardArray[i] == player2 && boardArray[i + 2] == player2 && boardArray[i + 4] == player2) ||
-                    (boardArray[i] == player1 && boardArray[i + 2] == player1 && boardArray[i + 4] == player1))
-                    return 1;
-            }
-
-            //check if it's a draw
-            if (boardArray[1] != '.' && boardArray[2] != '.' && boardArray[3] != '.' && boardArray[4] != '.' &&
-                boardArray[5] != '.' && boardArray[6] != '.' && boardArray[7] != '.' && boardArray[8] != '.' &&
-                boardArray[9] != '.')
-            {
-                return -1;
-            }
-            
-            return 0;
+        if (result == 1)
+        {
+            printTheMessage.PrintTheMessage($"Well done Player {roundCounter % 2 + 1} won the game!");
         }
 
-        private static int GetArrayIndex(string position)
+        if (result == -1)
         {
-            var coordinatesStrings = new string[11]{"0,0", "1,1", "1,2", "1,3", "2,1", "2,2", "2,3", "3,1", "3,2", "3,3", "q"};
+            printTheMessage.PrintTheMessage("Draw");
+        }
+    }
+}
+
+public class Players
+{
+        private const char _player1 = 'X';
+        private const char _player2 = 'O';
+        
+        public char Player1
+        {
+            get { return _player1; }
+        }
+
+        public char Player2
+        {
+            get { return _player2; }
+        }
+
+        public int GetArrayIndex(string position)
+        {
+            var coordinatesStrings = new string[11] {"0,0", "1,1", "1,2", "1,3", "2,1", "2,2", "2,3", "3,1", "3,2", "3,3", "q"};
             for (int i = 1; i < 10; i++)
             {
                 if (position == coordinatesStrings[i])
@@ -143,20 +198,24 @@ namespace Tic_Tac_Toe
             {
                 return 0;
             }
-
             return 10;
         }
 
-        private static void UpdateTokens(char player1, char player2, char[] boardArray, int roundCounter, int choiceOfPlayer)
-        {
+        public void UpdateTokens(char player1, char player2, char[] boardArray, int roundCounter, int choiceOfPlayer)
+        {    
+            
+            Board newBoard=new Board();
+            Print printTheMessage=new Print();
+            
             if (choiceOfPlayer == 0)
             {
                 roundCounter++;
-                PrintTheMessageWithNumber("Player {0} has given up for this round", (roundCounter % 2) + 1);
+                printTheMessage.PrintTheMessage($"Player {(roundCounter % 2) + 1} has given up for this round");
+                printTheMessage.PrintTheMessage("\n");
                 roundCounter += 2;
             }
 
-            if (choiceOfPlayer != 0 && boardArray[choiceOfPlayer] != player1 && boardArray[choiceOfPlayer] != player2)
+            if (choiceOfPlayer != 0 && boardArray[choiceOfPlayer] != player1 && boardArray[choiceOfPlayer] != player2 )
             {
                 if (roundCounter % 2 == 0)
                 {
@@ -167,25 +226,8 @@ namespace Tic_Tac_Toe
                 {
                     boardArray[choiceOfPlayer] = player1;
                 }
-                PrintTheMessage("Move accepted, here's the current board: ");
-                PrintBoard(boardArray);
+                printTheMessage.PrintTheMessage("Move accepted, here's the current board: ");
+                printTheMessage.PrintTheMessage("\n");
             }
-        }
-        
-        private static void CheckIfConditionIsWinning(int result, int roundCounter, char[] boardArray)
-        {
-            Console.Clear();
-            PrintBoard(boardArray);
-
-            if (result == 1)
-            {
-                PrintTheMessageWithNumber("Move accepted, well done Player {0} won the game!", (roundCounter % 2) + 1);
-            }
-
-            if (result == -1)
-            {
-                PrintTheMessage("Draw");
-            }
-        }
-    }
+        } 
 }
